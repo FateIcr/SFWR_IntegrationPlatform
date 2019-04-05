923ed4f227bb2675062884113f22e26f27213071
Rian <1450946020@qq.com>
2019-04-05 17:31:50 +0800

    完成初步框架架构，实现HelloWorld。

---
+++ src/main/java/com/consonance/sfwrip/SfwripApplication.java
@@ -0,0 +1,13 @@
+package com.consonance.sfwrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfwripApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfwripApplication.class, args);
+    }
+
+}
